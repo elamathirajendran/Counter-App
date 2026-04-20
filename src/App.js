40428e93ab274468286f904527e96642065c51a0
elamathirajendran <elamathirajendran91@gmail.com>
2026-04-20 12:32:53 +0530

    Debounce

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Counter } from "./Counter";
+import { DebounceInput } from "./DebounceInput";
 import { Fetch } from "./Fetch";
 
 function App() {
   return (
     <div className="App">
     <h1>Counter App</h1>
+    <DebounceInput/>
     <Counter/>
     <Fetch />
     </div>
--- src/DebounceInput.js
+++ src/DebounceInput.js
@@ -3,45 +3,46 @@ import React, { useEffect, useState } from 'react'
 export const DebounceInput = () => {
 
     const [searchValue,setSearchValue] = useState([]);
+   const [debouncesearchValue,setDebounceSearchValue] = useState([]);
    
     const [input,setInput] = useState("");
 
-    function Debounce(fn,delay){
+    useEffect(()=>{
 
-        let timeoutId=0;
-        return function(...args)
-        {
-            clearTimeout(timeoutId);
-            timeoutId= setTimeout(()=>{
-                fn(...args)
-            },delay);
-            
-        }
 
-    }
+        const timeout=setTimeout(() => {
+            setDebounceSearchValue(input);
+        }, 1000);
 
+        return ()=>clearTimeout(timeout);
+    },[input])
+
+   
 
      const fetchData= async(val)=>{
-        const res = await fetch(`https://jsonplaceholder.typicode.com/comments?email=${val}`);
+        const res = await fetch(`https://jsonplaceholder.typicode.com/comments?email=${input}`);
         const data = await res.json();
         setSearchValue(data);
 
     }
    
-const inputval= Debounce(fetchData,500);
+//const inputval= Debounce(fetchData,500);
    
 useEffect(()=>{
- inputval(input);
-    },[input,inputval])
+ fetchData(debouncesearchValue);
+    },[debouncesearchValue])
    
 
-
+const handleText=(text)=>{
+setInput(text);
+}
     
    
     return (
     <div><h1>DebounceInput</h1>
     <input type="text" placeholder='Search' value={input}
-    onChange={(e)=>setInput(e.target.value)} />
+    onChange={(e)=>handleText(e.target.value)} />
+    <span>{input}</span>
      <div><ul>
         {
             searchValue.map(item=> {
